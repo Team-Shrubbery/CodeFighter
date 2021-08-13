130f7d93cdb0eeaf06492198f9a1a32d2480036b
Davee Sok <daveesok@gmail.com>
2021-08-13 16:08:20 -0500

    pulling changes from dev

--- Game/data/player.py
+++ Game/data/player.py
@@ -15,35 +15,44 @@ class Player(pygame.sprite.Sprite):
         self.width = 145
         self.height = 125
 
-# --------------- Position and Direction -------------
+        # --------------- Position and Direction -------------
         self.vx = 0
         self.pos = vec((340, 240))
-        self.vel = vec(0,0)
-        self.acc = vec(0,0)
+        self.vel = vec(0, 0)
+        self.acc = vec(0, 0)
 
-# -------------- Movement --------------
+        # -------------- Movement --------------
         self.direction = "RIGHT"
         self.jumping = False
         self.running = False
         self.move_frame = 0
-        self.run_ani_R = [self.game.alucard_sprite_sheet.get_sprite(268, 795, self.width, self.height), self.game.alucard_sprite_sheet.get_sprite(75, 793, self.width, self.height),
-        self.game.alucard_sprite_sheet.get_sprite(54, 927, self.width, self.height)]
+        self.run_ani_R = [
+            self.game.alucard_sprite_sheet.get_sprite(268, 795, self.width, self.height),
+            self.game.alucard_sprite_sheet.get_sprite(75, 793, self.width, self.height),
+            self.game.alucard_sprite_sheet.get_sprite(54, 927, self.width, self.height),
+        ]
 
-        self.run_ani_L = [self.game.alucard_sprite_sheet.get_sprite(268, 795, self.width, self.height), self.game.alucard_sprite_sheet.get_sprite(75, 793, self.width, self.height),
-        self.game.alucard_sprite_sheet.get_sprite(54, 927, self.width, self.height)]
+        self.run_ani_L = [
+            self.game.alucard_sprite_sheet.get_sprite(268, 795, self.width, self.height),
+            self.game.alucard_sprite_sheet.get_sprite(75, 793, self.width, self.height),
+            self.game.alucard_sprite_sheet.get_sprite(54, 927, self.width, self.height),
+        ]
 
-
-# ----------- Combat ---------------------
+        # ----------- Combat ---------------------
         self.attacking = False
         self.cooldown = False
         self.attack_frame = 0
-        self.attack_ani_R = [self.game.alucard_sprite_sheet.get_sprite(30, 3866, self.width, self.height - 25),
-                            self.game.alucard_sprite_sheet.get_sprite(170, 3866, self.width, self.height - 25),
-                            self.game.alucard_sprite_sheet.get_sprite(314, 3860, self.width + 55, self.height),]
- 
-        self.attack_ani_L = [self.game.alucard_sprite_sheet.get_sprite(30, 3866, self.width, self.height - 25),
-                            self.game.alucard_sprite_sheet.get_sprite(170, 3866, self.width, self.height - 25),
-                            self.game.alucard_sprite_sheet.get_sprite(314, 3860, self.width + 55, self.height),]
+        self.attack_ani_R = [
+            self.game.alucard_sprite_sheet.get_sprite(30, 3866, self.width, self.height - 25),
+            self.game.alucard_sprite_sheet.get_sprite(170, 3866, self.width, self.height - 25),
+            self.game.alucard_sprite_sheet.get_sprite(314, 3860, self.width + 55, self.height),
+        ]
+
+        self.attack_ani_L = [
+            self.game.alucard_sprite_sheet.get_sprite(30, 3866, self.width, self.height - 25),
+            self.game.alucard_sprite_sheet.get_sprite(170, 3866, self.width, self.height - 25),
+            self.game.alucard_sprite_sheet.get_sprite(314, 3860, self.width + 55, self.height),
+        ]
 
     def move(self):
         self.acc = vec(0, 0.5)
@@ -52,7 +61,7 @@ class Player(pygame.sprite.Sprite):
         else:
             self.running = False
 
-# --------- keyboard input ----------------
+        # --------- keyboard input ----------------
         pressed_keys = pygame.key.get_pressed()
         if pressed_keys[K_LEFT]:
             self.acc.x = -ACC
@@ -85,8 +94,8 @@ class Player(pygame.sprite.Sprite):
                     self.pos.y = lowest.rect.top + 1
                     self.vel.y = 0
                     self.jumping = False
-    
-# ---------- method calls -----------
+
+    # ---------- method calls -----------
     def update(self):
         self.move()
         self.gravity_check()
@@ -99,21 +108,21 @@ class Player(pygame.sprite.Sprite):
 
         if self.jumping == False and self.running == True:
             if self.vel.x > 0:
-              if self.direction == "RIGHT" and self.running is False:
-                self.image = self.game.alucard_sprite_sheet.get_sprite(38, 179, 145, 125)
-                self.image.set_colorkey(MAGENTA)
-              else:
-                self.image = self.run_ani_R[math.floor(self.move_frame)]
-                self.image.set_colorkey(MAGENTA)
+                if self.direction == "RIGHT" and self.running is False:
+                    self.image = self.game.alucard_sprite_sheet.get_sprite(38, 179, 145, 125)
+                    self.image.set_colorkey(MAGENTA)
+                else:
+                    self.image = self.run_ani_R[math.floor(self.move_frame)]
+                    self.image.set_colorkey(MAGENTA)
             elif self.vel.x < 0:
-              if self.direction == "LEFT" and self.running is False:
-                self.image = self.game.alucard_sprite_sheet.get_sprite(38, 179, 145, 125)
-                self.image = pygame.transform.flip(self.image, True, False)
-                self.image.set_colorkey(MAGENTA)
-              else:
-                self.image = self.run_ani_L[math.floor(self.move_frame)]
-                self.image.set_colorkey(MAGENTA)
-                self.image = pygame.transform.flip(self.image, True, False)
+                if self.direction == "LEFT" and self.running is False:
+                    self.image = self.game.alucard_sprite_sheet.get_sprite(38, 179, 145, 125)
+                    self.image = pygame.transform.flip(self.image, True, False)
+                    self.image.set_colorkey(MAGENTA)
+                else:
+                    self.image = self.run_ani_L[math.floor(self.move_frame)]
+                    self.image.set_colorkey(MAGENTA)
+                    self.image = pygame.transform.flip(self.image, True, False)
 
             self.move_frame += 0.1
 
@@ -124,13 +133,12 @@ class Player(pygame.sprite.Sprite):
             elif self.direction == "LEFT":
                 self.image = self.run_ani_L[self.move_frame]
 
-# ----- correction for pygame attack error when attacking left ----------
+    # ----- correction for pygame attack error when attacking left ----------
     def correction(self):
         if self.attack_frame == 1:
             self.pos.x -= 20
         if self.attack_frame == 4:
             self.pos.x += 20
-
 
     def attack(self):
         if self.attack_frame > 3:
@@ -150,8 +158,8 @@ class Player(pygame.sprite.Sprite):
     # def player_hit(self):
     #     hits = pygame.sprite.spritecollide(self, self.player, False)
     #     if self.cooldown == False:
-    #         self.cooldown = True 
-    #         pygame.time.set_timer(self.hit_cooldown, 1000) 
+    #         self.cooldown = True
+    #         pygame.time.set_timer(self.hit_cooldown, 1000)
 
     #         pygame.display.update()
 
@@ -160,7 +168,7 @@ class Player(pygame.sprite.Sprite):
         hits = pygame.sprite.spritecollide(self, self.game.ground_group, False)
 
         self.rect.x -= 1
-        
+
         if hits and not self.jumping:
             self.jumping = True
             self.vel.y = -12
--- Game/main.py
+++ Game/main.py
@@ -6,64 +6,65 @@ from data.ground import *
 
 # ------------------------ Main Game Class
 class Game:
-      def __init__(self):
-            self.screen = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
-            pygame.display.set_caption("Code Fighter")
-            # self.font = pygame.font.Font("resources/blackbone-font/Blackbone-0WxXR.ttf", 64)
-            self.clock = pygame.time.Clock()
-            self.playing = True
+    def __init__(self):
+        self.screen = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+        pygame.display.set_caption("Code Fighter")
+        # self.font = pygame.font.Font("resources/blackbone-font/Blackbone-0WxXR.ttf", 64)
+        self.clock = pygame.time.Clock()
+        self.playing = True
 
-# ----------------------- Sprite Sheets and image files to load ---------------------------
-            self.background = pygame.image.load('resources/img/Battleback1.png')
-            self.ground_image = pygame.image.load('resources/img/Ground.png')
-            self.alucard_sprite_sheet = Spritesheet('resources/img/alucardfinal.png')
+        # ----------------------- Sprite Sheets and image files to load ---------------------------
+        self.background = pygame.image.load("resources/img/Battleback1.png")
+        self.ground_image = pygame.image.load("resources/img/Ground.png")
+        self.alucard_sprite_sheet = Spritesheet("resources/img/alucardfinal.png")
 
-# ----------------------- Putting sprites into groups and instantiatiating objects
-      def new(self):
-            self.all_sprites = pygame.sprite.LayeredUpdates()
-            self.ground_group = pygame.sprite.LayeredUpdates()
-            self.player = pygame.sprite.LayeredUpdates()
-            self.ground = Ground(self)
-            self.player = Player(self)
+    # ----------------------- Putting sprites into groups and instantiatiating objects
+    def new(self):
+        self.all_sprites = pygame.sprite.LayeredUpdates()
+        self.ground_group = pygame.sprite.LayeredUpdates()
+        self.player = pygame.sprite.LayeredUpdates()
+        self.ground = Ground(self)
+        self.player = Player(self)
 
-# ------------------------ Update every sprite in the game/added to all_sprites group -------------------
-      def update(self):
-            self.all_sprites.update()
-            
-# ------------------------ Draws on the screen ---------------------------
-      def draw(self):
-            self.screen.fill(BLACK)
-            self.screen.blit(self.background, (0, 0))
-            self.all_sprites.draw(self.screen)
-            self.clock.tick(FPS)
-            pygame.display.update()
+    # ------------------------ Update every sprite in the game/added to all_sprites group -------------------
+    def update(self):
+        self.all_sprites.update()
 
-# -------------------------  Main game loop and update calls --------------
-      def main(self):
-            while self.playing:
-                  self.events()
-                  self.update()
-                  self.draw()
-            self.playing = False
+    # ------------------------ Draws on the screen ---------------------------
+    def draw(self):
+        self.screen.fill(BLACK)
+        self.screen.blit(self.background, (0, 0))
+        self.all_sprites.draw(self.screen)
+        self.clock.tick(FPS)
+        pygame.display.update()
 
-# -------------------------- handles different game events, can look up events in pygame docs ------------
-      def events(self):
-            for event in pygame.event.get():
-                  if event.type == QUIT:
-                        self.playing = False
-                  # For events that occur upon clicking the mouse (left click) 
-                  if event.type == pygame.MOUSEBUTTONDOWN:
-                        pass
-            
-                  # Event handling for a range of different key presses    
-                  if event.type == pygame.KEYDOWN:
-                        pass
+    # -------------------------  Main game loop and update calls --------------
+    def main(self):
+        while self.playing:
+            self.events()
+            self.update()
+            self.draw()
+        self.playing = False
+
+    # -------------------------- handles different game events, can look up events in pygame docs ------------
+    def events(self):
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                self.playing = False
+            # For events that occur upon clicking the mouse (left click)
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pass
+
+            # Event handling for a range of different key presses
+            if event.type == pygame.KEYDOWN:
+                pass
+
 
 # ------ starting the game --------
 g = Game()
 g.new()
 while g.playing is True:
-      g.main()
+    g.main()
 
 pygame.quit()
 sys.exit()
