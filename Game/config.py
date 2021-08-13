591c63fcbaf89e5f884b1500ffcbeeedb03b0a48
Glenn Elliron Clark <Elliron.Clark@gmail.com>
2021-08-13 12:27:23 -0700

    refactored/cleaned files and code

--- Game/config.py
+++ Game/config.py
@@ -1,11 +1,22 @@
-WIN_WIDTH = 800
-WIN_HEIGHT = 600
+import pygame
 
-BLACK = (0,0,0)
-RED = (255, 0, 0)
-MAGENTA = (255, 0, 255)
+vec = pygame.math.Vector2
+
+WIN_HEIGHT = 350
+WIN_WIDTH = 700
+
+ACC = 0.3
+FRIC = -0.10
 
 FPS = 60
 
-PLAYER_LAYER = 1
-PLAYER_SIZE = 144
+COUNT = 0
+
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+MAGENTA = (255, 0, 255)
+
+GROUND_LAYER = 1
